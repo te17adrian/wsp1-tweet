c1b628d8362a93f11a321bae3689a780129f3101
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2019-09-12 10:54:19 +0200

    added index_layout.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-  
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Den fantastiska tweetsidan!</h1>
 <?php
 include 'include/dbinfo.php';
 try {
@@ -27,15 +15,6 @@ $sth = $dbh->prepare('SELECT * FROM tweet
                 ON tweet.user_id =users.id');
 $sth->execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-//echo "<pre>" . print_r($result,1) . "</pre>";
- foreach ($result as $row) {
-     //echo "<pre>" . print_r($row,1) . "</pre>";
-     echo "<div>";
-     echo "<h2>" . $row['body'] . "</h2>";
-     echo "<p>" . $row['name'] . "</p>";
-     echo "</div>";
- }
+
+include 'views/index_layout.php'
 ?>
-    </div>
-</body>
-</html>

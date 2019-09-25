7214c0b890aefd513abd2bbc5d718e8a2a3f8a75
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2019-09-25 09:37:09 +0200

    Mer php filer för organisering

--- views/card.php
+++ views/card.php
@@ -1,30 +1,23 @@
-<?php include 'head.php' ?>
-<body>
-    <div class="container px-5">
-    <?php foreach ($result as $row): ?>
-        <section class="card mt-2">
+<section class="card mt-2">
             <header class="card-header d-flex align-items-center">
-            <img class="img-thumbnail" src="https://robohash.org/<?= $row['name'] ?>?set=set2" alt="pfp">
+                    <img class="img-thumbnail" src="https://robohash.org/<?= $result['name'] ?>?set=set2" alt="pfp">
                 <h5 class="card-title pl-4 mb-0">
-                    <?= $row['name'] ?>
+                    <?= $result['name'] ?>
                     <small class="pl-4">
-                        <?= $row['created_at'] ?>
+                        <?= $result['created_at'] ?>
                     </small>
                 </h5>
             </header>
-            <a href="tweet.php?id=<?= $row['id'] ?>">
+            <a href="index.php">
                 <div class="card-body">
                     <p class="card-text">
-                        <?= $row['body'] ?>
+                        <?= $result['body'] ?>
                     </p>
                 </div>
             </a>
-            <footer class="card-footer d-flex ">
+            <footer class="card-footer d-flex">
             <button type="button" class="btn btn-dark">Like</button>
             <button type="button" class="btn btn-dark">Comment</button>
             <button type="button" class="btn btn-dark">Retweet</button>
             </footer>
         </section>
-    <?php endforeach ?>
-    </div>
-    <?php include 'foot.php' ?>
--- views/tweet_layout.php
+++ views/tweet_layout.php
@@ -1,41 +1,5 @@
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tweet</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
+<?php include 'head.php' ?>
     <div class="container px-5">
-        <section class="card mt-2">
-            <header class="card-header d-flex align-items-center">
-                    <img class="img-thumbnail" src="https://robohash.org/<?= $result['name'] ?>?set=set2" alt="pfp">
-                <h5 class="card-title pl-4 mb-0">
-                    <?= $result['name'] ?>
-                    <small class="pl-4">
-                        <?= $result['created_at'] ?>
-                    </small>
-                </h5>
-            </header>
-            <a href="index.php">
-                <div class="card-body">
-                    <p class="card-text">
-                        <?= $result['body'] ?>
-                    </p>
-                </div>
-            </a>
-            <footer class="card-footer d-flex">
-            <button type="button" class="btn btn-dark">Like</button>
-            <button type="button" class="btn btn-dark">Comment</button>
-            <button type="button" class="btn btn-dark">Retweet</button>
-            </footer>
-        </section>
+<?php include 'card.php' ?>
     </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include 'foot.php' ?>
